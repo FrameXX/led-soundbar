43e4e9f533ac3ff0fffc3927e1bf8d8eefdde420
FrameXX <jirkakral978@gmail.com>
2024-03-25 19:16:46 +0100

    Fix dataPin argument not being resolved at compile time even though it has to.

--- lib/LEDSoundbar/LEDSoundbar.h
+++ lib/LEDSoundbar/LEDSoundbar.h
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
+template <uint8_t DataPin>
 class LEDSoundbar
 {
 private:
@@ -11,12 +12,47 @@ private:
   CRGB maxVolumeColor;
   CRGB *LEDs;
 
-  CRGB getVolumeColor(int volume);
-
 public:
-  LEDSoundbar(uint8_t dataPin, int LEDCount, CRGB minVolumeColor, CRGB maxVolumeColor);
+  LEDSoundbar(int LEDCount, CRGB minVolumeColor, CRGB maxVolumeColor) : LEDCount(LEDCount), minVolumeColor(minVolumeColor), maxVolumeColor(maxVolumeColor)
+  {
+    this->LEDs = new CRGB[LEDCount];
+    FastLED.addLeds<NEOPIXEL, DataPin>(this->LEDs, LEDCount);
+  }
 
-  ~LEDSoundbar();
+  ~LEDSoundbar()
+  {
+    delete[] LEDs;
+  }
 
-  void reportVolume(int volume);
+  void reportVolume(int volume)
+  {
+    const CRGB enabledColor = this->getVolumeColor(volume);
+    const int topEnabledLEDIndex = map(volume, 0, 1024, 0, this->LEDCount) - 1;
+
+    if (topEnabledLEDIndex != -1)
+    {
+      for (int i = 0; i <= topEnabledLEDIndex; i++)
+      {
+        this->LEDs[i] = enabledColor;
+      }
+    }
+
+    if (topEnabledLEDIndex != this->LEDCount - 1)
+    {
+      for (int i = topEnabledLEDIndex + 1; i <= this->LEDCount - 1; i++)
+      {
+        this->LEDs[i] = CRGB::Black;
+      }
+    }
+
+    FastLED.show();
+  }
+
+  CRGB getVolumeColor(int volume)
+  {
+    const char r = map(volume, 0, 1024, this->minVolumeColor.r, this->minVolumeColor.r);
+    const char g = map(volume, 0, 1024, this->minVolumeColor.g, this->minVolumeColor.g);
+    const char b = map(volume, 0, 1024, this->minVolumeColor.b, this->minVolumeColor.b);
+    return CRGB(r, g, b);
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include <Clamp.h>
 #include <Amplify.h>
 
-#define LED_PIN1 1
-#define LED_PIN2 2
+#define LED_PIN1 3
+#define LED_PIN2 4
 #define NUM_LEDS 16
 
 #define MICROPHONE_PIN A0
-#define MIN_VOLUME_VOLTAGE 0
-#define MAX_VOLUME_VOLTAGE 512
+#define MIN_VOLUME_VOLTAGE 32
+#define MAX_VOLUME_VOLTAGE 64
 #define VOLUME_AMPLIFY_EXPONENT 1
 
 #define DELAY_MS 10
@@ -18,7 +18,7 @@
 CRGB minVolumeColor(128, 128, 255);
 CRGB maxVolumeColor(255, 128, 128);
 
-LEDSoundbar soundbar1(LED_PIN1, NUM_LEDS, minVolumeColor, maxVolumeColor);
+LEDSoundbar<LED_PIN1> soundbar1(NUM_LEDS, minVolumeColor, maxVolumeColor);
 
 int normalizeVolume(int volume, int minVolumeVoltage, int maxVolumeVoltage)
 {
